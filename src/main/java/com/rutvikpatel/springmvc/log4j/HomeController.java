026768f04de949ad556fcec538d4b056851ee486
Rutvik Patel <rutvikrpatel@users.noreply.github.com>
2017-07-24 16:26:08 +0530

    Add files via upload

---
+++ src/main/java/com/rutvikpatel/springmvc/log4j/HomeController.java
@@ -0,0 +1,36 @@
+package com.rutvikpatel.springmvc.log4j;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		String formattedDate = dateFormat.format(date);
+		model.addAttribute("serverTime", formattedDate );
+		
+		logger.info("<< Info - Welcome home! Time : "+formattedDate);
+		logger.warn("<< WARN - Welcome home! Time : "+formattedDate);
+		logger.debug("<< DEBUG - Welcome home! Time : "+formattedDate);
+		
+		return "home";
+	}
+}
